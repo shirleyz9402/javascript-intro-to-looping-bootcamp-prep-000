696a2d9e23ed33ba927be2531cc7cb63cea078d0
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-08 18:42:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,6 +10,7 @@ for (var i = 0; i < 25; i++)
 function whileLoop(n){
 let countdown = n 
 while (countdown > 0) {
+<<<<<<< HEAD
   console.log(--countdown)
   }
  return "done"
@@ -22,4 +23,10 @@ function doWhileLoop(array){
     array.slice(0, array.length - 1)
   } while (array.length > 0 && maybeTrue())
   return array && "false"
+=======
+  --countdown
+  console.log("done")
+  }
+
+>>>>>>> 04ff6349598dae75fd6a10e9ae3080ef5e5aa7e4
 }
